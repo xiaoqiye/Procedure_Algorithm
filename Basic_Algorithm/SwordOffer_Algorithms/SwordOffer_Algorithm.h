03a5712dcd656bc12a023001e698bb176a4a6dd1
小七爷 <806707499@qq.com>
2021-06-16 23:25:59 +0800

    modify

--- Basic_Algorithm/SwordOffer_Algorithms/SwordOffer_Algorithm.h
+++ Basic_Algorithm/SwordOffer_Algorithms/SwordOffer_Algorithm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 class CSwordOfferAlgorithm
 {
@@ -9,7 +10,11 @@ public:
 	{
 	public:
 		CMyString(char* vStr = nullptr) {}
-		CMyString(const CMyString& vStr) {}
+		CMyString(const CMyString& vStr) {
+			_ASSERTE(m_pStr == nullptr);
+			m_pStr = new char[strlen(vStr.m_pStr) + 1];
+			strcpy_s(m_pStr, sizeof(vStr.m_pStr), vStr.m_pStr);
+		}
 		~CMyString() {}
 
 		//初级程序员方法
